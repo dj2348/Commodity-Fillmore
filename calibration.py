7e0ea7ee4a29b4570d0607e3e2d7d95461cba819
Fuzhong Zhou <40592542+fuzhongzhou@users.noreply.github.com>
2020-10-11 14:47:09 -0400

    Update Fourier transform

--- calibration.py
+++ calibration.py
@@ -3,16 +3,17 @@ from numpy import exp, sqrt, log, pi
 from numpy.fft import fft
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.interpolate import CubicSpline, interp1d
 
 lam_gas = 4.4
-span = 126
+span = 120
 delta_t = 1/252
 
-gas_hisspot = pd.read_csv('Henry_Hub_Natural_Gas_Spot_Price.csv')
+gas_hisspot = pd.read_excel('gas spot.xlsx')
 gas_hisspot.columns = ['Day', 'Spot']
 gas_hisspot['Day'] = pd.to_datetime(gas_hisspot['Day'])
 gas_hisspot = gas_hisspot.set_index('Day').sort_index()
-gas_hisspot = gas_hisspot.loc['2006-01-01':'2008-04-01']
+#gas_hisspot = gas_hisspot.loc['2006-01-01':'2008-04-01']
 
 gas_hisspot_exp = gas_hisspot.copy()
 gas_hisspot_exp['t'] = np.arange(gas_hisspot.shape[0]) / 252
@@ -34,7 +35,7 @@ gas_hisvol = gas_hisvol.dropna()
 print(gas_hisvol)
 #gas_hisvol.plot()
 
-y = np.fft.fft(gas_hisvol.loc['2006-01-01':'2008-04-01'])
+y = np.fft.fft(gas_hisvol)
 y_real = np.real(y).squeeze()
 y_imag = np.imag(y).squeeze()
 freq = np.fft.fftfreq(y.shape[0])
@@ -46,26 +47,27 @@ plt.plot(freq, y_real)
 plt.show()
 '''
 
-n = 3
-freq_ = freq[y_real.argsort()[::20][-n:]]
-
-
-x = np.linspace(0, len(y)/252, len(y))
-
-
+n = 10
+sep = 50
+freq_ = freq[y_real.argsort()[::sep][-n:]]
+time = np.linspace(0, len(gas_hisvol)/365, len(gas_hisvol))
+midtime = np.median(time)
+endtime = time.max()
+time_shift = time - endtime
 def volatility(x):
-    fitted = 5
+    fitted = 4
     for i in range(n):
-        fitted += 0.5 * np.cos(2*pi*(freq_[i]+0.13) * (x + 1.73015873) - 1.6)
+        fitted += 1 / n * np.cos(2*pi * (freq_[i]) * (x + midtime))
     return fitted
 
-x = np.linspace(0, len(gas_hisvol['Vol'])/252, len(gas_hisvol['Vol'])) - 1.73015873
 
+x = np.linspace(-endtime, 1, 1000)
 gas_hisvol_fitted = volatility(x)
+
 
 fig = plt.figure()
 ax = fig.subplots(1)
-plt.plot(x, np.array(gas_hisvol['Vol']))
+plt.plot(time_shift, np.array(gas_hisvol['Vol']))
 plt.plot(x, gas_hisvol_fitted)
 plt.show()
 
